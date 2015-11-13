99f5503f42ac35a102287df52ee59a41497d9e81
mupini <ranie@ransomempini.com>
2015-11-13 14:03:22 +0000

    update idt datasets endpoint

--- tasks/update_from_idt.js
+++ tasks/update_from_idt.js
@@ -216,10 +216,11 @@ module.exports = function (grunt) {
             grunt.log.writeln('Got here');
             var request = {
                 host: 'www.bbc.co.uk',
-                path: '/indepthtoolkit/data_set/jhadi-facewall'
-            }
+                path: '/indepthtoolkit/data-sets/jhadi-facewall'
+            };
             grunt.log.writeln(JSON.stringify(getHttpOptions(request)));
             http.request(getHttpOptions(request), function (res) {
+
                 var str = '';
 
                 res.on('data', function (chunk) {
